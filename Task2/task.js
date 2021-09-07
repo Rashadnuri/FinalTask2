0e19630b174a28c2bb60dc01c3be3c51e042caa7
Rashadnuri <86822651+Rashadnuri@users.noreply.github.com>
2021-09-07 23:22:22 +0400

    Initial commit

---
+++ Task2/task.js
@@ -0,0 +1,7 @@
+const human={
+ name:"Rashad",
+ age:"19",
+ country:"Azerbaijan"
+} ;
+
+console.log(human.name, huma.age, human.country)
---
+++ Task2/task2.js
@@ -0,0 +1,26 @@
+let param1 =['alma', 'armud']
+
+const param2 = {
+    ad:'alma',
+    reng:'qirmizi',
+}
+
+param3=3
+
+param4=necesen
+
+function task(param1,param2,param3,param4) { 
+    if (param1=Array){
+        console.log(param1)
+    }
+    if (param2=Object){
+        console.log(Object.getOwnPropertyNames(param2));
+    }
+    if (param3=Number){
+        console.log(param3*param3)
+    }
+    
+    if (param4=String){
+        console.log(param4)
+    };
+}
